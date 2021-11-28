4f53e47edb70fa74db39369a3775a1c1e3b3efda
Arpit <arpitbabbar01@gmail.com>
2021-11-29 04:35:56 +0530

    Added Models and Routes

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,16 @@ connect();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello Bro!')
-})
+app.use(express.json());
+
+//Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello Bro!')
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
