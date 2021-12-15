4dc6b0acbd2c095efd64396cca737329a12bb448
Arpit <arpitbabbar01@gmail.com>
2021-12-15 20:46:07 +0530

    Componentys and Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 function App() {
   return (
-    <h1>Hey</h1>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <>
+            <h>This is aBout</h>
+        </>
+    )
+}
+
+export default About;
